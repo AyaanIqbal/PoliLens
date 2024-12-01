dd318dc2c4ea81d8972b1d4b2520a335737e2a78
AyaanIqbal <ayaaniqbal@gmail.com>
2024-11-30 23:57:15 -0500

    added congress members to database (#11)

--- mongo_db_manager.py
+++ mongo_db_manager.py
@@ -13,15 +13,18 @@ client = pymongo.MongoClient(os.getenv('MONGO_URI'))
 # Call scraping functions for trades and bills
 trade_df = td.scrape_and_transform_transactions()
 bill_df = bd.fetch_bills(116, 'HR')
+congress_df = bd.fetch_congress_members(116)
 
 # Specify database and collections
 db = client["polilens_data"]
 trade_collection = db['trades']
 bill_collection = db['bills']
+congress_collection = db['congress-members']
 
-# Convert trade and bills DataFrame to dictionary for MongoDB insertion
+# Convert trade, bills, congress members DataFrame to dictionary for MongoDB insertion
 trade_data_dict = trade_df.to_dict("records")
 bill_data_dict = bill_df.to_dict("records")
+congress_data_dict = congress_df.to_dict("records")
 
 # Insert trade data into MongoDB collection
 try:
@@ -36,6 +39,13 @@ try:
     print("Bill data successfully inserted into MongoDB.")
 except Exception as e:
     print(f"An error occurred while inserting bill data: {e}")
+
+# Insert congress data into MongoDB collection
+try:
+    congress_collection.insert_many(congress_data_dict)
+    print("Congress data successfully inserted into MongoDB.")
+except Exception as e:
+    print(f"An error occurred while inserting congress data: {e}")
 
 # Check if the database exists
 dblist = client.list_database_names()
